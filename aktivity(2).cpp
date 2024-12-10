d3966f3cc91402e0b1ec137447a72d2172d20e05
syafutra56 <syafutradmk@gmail.com>
2024-12-10 11:08:57 +0700

    Add files via upload

---
+++ aktivity(2).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book {
+public:
+    string title;
+    string author;
+    double price;
+
+   
+    Book(string t, string a, double p) {
+        title = t;
+        author = a;
+        price = p;
+    }
+
+    void displayDetails() {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: Rp" << price << endl << endl;
+    }
+};
+
+int main() {
+    
+    Book book1("lovesick girl", "blacpink", 15000);
+    Book book2("better up", "babymonster", 70000);
+    book1.displayDetails();
+    book2.displayDetails();
+
+    return 0;
+}
